84af49587d5913f30575dda18c5f2dac07b4e7fb
aeon <jerry73204@gmail.com>
2025-10-31 01:51:24 +0800

    Rewrite CARLA simulator control script

--- scripts/simulators/startup/run-0.9.16.sh
+++ scripts/simulators/startup/run-0.9.16.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 set -e
 script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-cd "$script_dir/../carla-0.9.16"
+cd "$script_dir/carla-0.9.16"
 
 export VK_ICD_FILENAMES=/usr/share/vulkan/icd.d/nvidia_icd.json
 export DISPLAY=:1
 
-./CarlaUE4.sh -quality-level=Low -carla-rpc-port=3000
+PORT="${CARLA_PORT:-3000}"
+./CarlaUE4.sh -quality-level=Low -carla-rpc-port=$PORT
