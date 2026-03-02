abe2db0135ad8081f7546d39d05e556ae9d137bf
aeon <jerry73204@gmail.com>
2026-03-02 08:47:50 +0800

    Install clang-13 libclang-13-dev in install_deps.sh

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -6,6 +6,6 @@ pip install --user colcon-cargo-ros2
 
 echo "Installing system dependencies..."
 sudo apt-get update
-sudo apt-get install -y libclang-dev python3-vcstool
+sudo apt-get install -y clang-13 libclang-13-dev python3-vcstool
 
 echo "Dependencies installed successfully!"
